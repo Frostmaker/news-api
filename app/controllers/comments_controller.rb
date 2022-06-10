2c9b6abd7327b2c36b790e61ddf2087474681367
Miyagi <falin.original@gmail.com>
2022-06-10 16:25:35 +0300

    Add comments to post, but only creation of a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,27 @@ class CommentsController < ApplicationController
 	def show
 	end
 
-	private
+	def new
+		@comment = Comment.new
+	end
 
+	def create 
+		@post = Post.find(params[:post_id])
+
+		respond_to do |format|
+			if @post.comments.create(comment_params)
+			  format.html { redirect_to post_path(@post), notice: "Comment was successfully created." }
+			else
+			  format.html { redirect_to post_path(@post), status: :unprocessable_entity }
+			end
+		end
+	end
+	private
 	def set_comment 
 		@comment = Comment.find(params[:id])
 	end
 
-	def comment
+    def comment_params
+      params.require(:comment).permit(:author, :message)
+    end
 end
